641a2fb2089c13bf14964ce41043b3451b088b0a
Alex Tetervak <tetervak@sheridancollege.ca>
2024-03-14 21:05:18 -0400

    preserved the roll state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {RollerService} from "./roller.service";
 import {RollData} from "./roll-data";
+import {DataStorageService} from "./data-storage.service";
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,18 @@ export class AppComponent {
   // the input value
   numberOfDice: number = 3;
 
-  constructor(private rollerService: RollerService) {
+  constructor(private rollerService: RollerService, private dataStorageService: DataStorageService) {
+    if(dataStorageService.isRollDataSaved()){
+      let rollData: RollData = dataStorageService.loadRollData();
+      this.rollData = rollData;
+      this.numberOfDice = rollData.numberOfDice;
+    }
   }
 
   onRollDice(): void {
-    this.rollData = this.rollerService.getRollData(this.numberOfDice);
+    let rollData: RollData = this.rollerService.getRollData(this.numberOfDice);
+    this.rollData = rollData;
+    this.dataStorageService.saveRollData(rollData);
   }
 
 }
---
+++ src/app/data-storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {RollData} from "./roll-data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+
+  private static readonly key: string = "dice-values";
+  saveRollData(rollData: RollData): void {
+    localStorage[DataStorageService.key] = JSON.stringify(rollData.values);
+  }
+
+  loadRollData(): RollData{
+    let values = JSON.parse(localStorage[DataStorageService.key]);
+    return {
+      values: values,
+      total: values.reduce((a: number, b: number) => a + b, 0),
+      numberOfDice: values.length
+    };
+  }
+
+  isRollDataSaved(): boolean{
+    return localStorage.getItem(DataStorageService.key) != null;
+  }
+}
