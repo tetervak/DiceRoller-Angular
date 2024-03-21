13910c204ad6a3d3bb83ff58a4ee70e4bda32012
Alex Tetervak <tetervak@sheridancollege.ca>
2024-03-21 09:26:08 -0400

    removed an unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {MatInputModule} from "@angular/material/input";
-import {MatCardModule} from "@angular/material/card";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
@@ -32,7 +31,6 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     MatToolbarModule,
     MatButtonModule,
     MatFormFieldModule, MatSelectModule, MatInputModule,
-    MatCardModule,
     MatProgressSpinnerModule
   ],
   providers: [
